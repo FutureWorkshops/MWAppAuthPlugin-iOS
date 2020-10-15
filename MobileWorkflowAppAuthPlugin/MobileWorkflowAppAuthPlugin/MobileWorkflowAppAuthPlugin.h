f74621f5e6cad896fe64310a203b847e40709670
Jonathan Flintham <jonathan@futureworkshops.com>
2020-10-15 16:06:37 +0100

    Project setup

---
+++ MobileWorkflowAppAuthPlugin/MobileWorkflowAppAuthPlugin/MobileWorkflowAppAuthPlugin.h
@@ -0,0 +1,18 @@
+//
+//  MobileWorkflowAppAuthPlugin.h
+//  MobileWorkflowAppAuthPlugin
+//
+//  Created by Jonathan Flintham on 15/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileWorkflowAppAuthPlugin.
+FOUNDATION_EXPORT double MobileWorkflowAppAuthPluginVersionNumber;
+
+//! Project version string for MobileWorkflowAppAuthPlugin.
+FOUNDATION_EXPORT const unsigned char MobileWorkflowAppAuthPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileWorkflowAppAuthPlugin/PublicHeader.h>
+
+
