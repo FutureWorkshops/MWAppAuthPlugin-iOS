2cf1e6380e71f2595812458c7889ab7952f6bb69
Jonathan Flintham <jonathan@futureworkshops.com>
2021-10-08 17:17:47 +0100

    Moved from AppAuth to ASWebAuthenticationSession

--- MWAppAuthPlugin/MWAppAuthPlugin/AppAuthStep/MWAppAuthStepViewController+AppAuth.swift
+++ MWAppAuthPlugin/MWAppAuthPlugin/AppAuthStep/MWAppAuthStepViewController+AppAuth.swift
@@ -7,61 +7,131 @@
 
 import UIKit
 import MobileWorkflowCore
-import AppAuth
 import AuthenticationServices
+
+struct WebAuthenticationRequest: URLQuery {
+    private enum Constants {
+        static let redirectPath = "://callback"
+    }
+    let clientId: String
+    let scope: String
+    let redirectUri: String
+    let responseType: String = "code"
+    
+    init(config: OAuth2Config) {
+        self.clientId = config.oAuth2ClientId
+        self.scope = config.oAuth2Scope
+        self.redirectUri = config.oAuth2RedirectScheme.appending(Constants.redirectPath)
+    }
+}
+
+struct OAuthAccessTokenResponse: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+        case expiresIn = "expires_in"
+        case scope = "scope"
+        case refreshToken = "refresh_token"
+    }
+    
+    let accessToken: String
+    let tokenType: String
+    let expiresIn: Int
+    let scope: String
+    let refreshToken: String?
+}
 
 extension MWAppAuthStepViewController {
     
     func performOAuth(config: OAuth2Config) {
+        let webAuthorizationRequest = WebAuthenticationRequest(config: config)
         guard
             let oAuth2Url = self.appAuthStep.session.resolve(url: config.oAuth2Url)?.absoluteString,
-            let authorizationEndpoint = URL(string: oAuth2Url.appending(OAuthPaths.authorization)),
-            let tokenEndpoint = URL(string: oAuth2Url.appending(OAuthPaths.token)),
-            let redirectURL = URL(string: config.oAuth2RedirectScheme + "://callback")
-            else { return }
+            let authorizationURL = webAuthorizationRequest.queryURL(for: oAuth2Url.appending(OAuthPaths.authorization)),
+            let tokenURL = URL(string: oAuth2Url.appending(OAuthPaths.token))
+        else { return }
         
         self.showLoading()
         
-        let configuration = OIDServiceConfiguration(
-            authorizationEndpoint: authorizationEndpoint,
-            tokenEndpoint: tokenEndpoint
-        )
-        
-        let request = OIDAuthorizationRequest(
-            configuration: configuration,
-            clientId: config.oAuth2ClientId,
-            clientSecret: (config.oAuth2ClientSecret?.isEmpty ?? true) ? nil : config.oAuth2ClientSecret,
-            scopes: [config.oAuth2Scope],
-            redirectURL: redirectURL,
-            responseType: OIDResponseTypeCode,
-            additionalParameters: nil
-        )
-        
-        let authProvider = AuthProviderImplementation() { completion in
-            let session = OIDAuthState.authState(byPresenting: request, presenting: self) { authState, error in
-                if let authState = authState, let accessToken = authState.lastTokenResponse?.accessToken, let expirationDate = authState.lastTokenResponse?.accessTokenExpirationDate {
-                    let token = Credential(
-                        type: CredentialType.token.rawValue,
-                        value: accessToken,
-                        expirationDate: expirationDate
-                    )
-                    var refresh: Credential?
-                    if let refreshToken = authState.lastTokenResponse?.refreshToken {
-                        refresh = Credential(
-                            type: CredentialType.refreshToken.rawValue,
-                            value: refreshToken,
-                            expirationDate: .distantFuture
-                        )
-                    }
-                    completion(.success([token, refresh].compactMap({ $0 })))
-                } else {
-                    completion(.failure(error ?? CredentialError.unexpected))
-                }
+        let authProvider = AuthProviderImplementation() { [weak self] completion in
+            guard let strongSelf = self else {
+                completion(.failure(URLError(.cancelled)))
+                return nil
             }
+            
+            let session = ASWebAuthenticationSession(
+                url: authorizationURL,
+                callbackURLScheme: config.oAuth2RedirectScheme,
+                completionHandler: { [weak self] callbackURL, error in
+                    guard let session = self?.appAuthStep.session,
+                          let services = self?.appAuthStep.services
+                    else { return }
+                    
+                    if let error = error {
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    guard let callbackURL = callbackURL,
+                          let queryItems = NSURLComponents(string: callbackURL.absoluteString)?.queryItems,
+                          let code = queryItems.filter({$0.name == "code"}).first?.value
+                    else {
+                        completion(.failure(URLError(.badServerResponse)))
+                        return
+                    }
+                    
+                    var params: [String: String] = [
+                        "grant_type": "authorization_code",
+                        "client_id": config.oAuth2ClientId,
+                        "redirect_uri": config.oAuth2RedirectScheme + "://callback",
+                        "code": code
+                    ]
+                    if let secret = config.oAuth2ClientSecret {
+                        params["client_secret"] = secret
+                    }
+                    let paramsString = params.map({ "\($0.key)=\($0.value)" }).joined(separator: "&") // url encoding
+                    
+                    let tokenTask = URLAsyncTask<OAuthAccessTokenResponse>.build(
+                        url: tokenURL,
+                        method: .POST,
+                        body: paramsString.data(using: .utf8),
+                        session: session,
+                        headers: ["Content-Type": "application/x-www-form-urlencoded"],
+                        parser: { try OAuthAccessTokenResponse.parse(data: $0) }
+                    )
+                    
+                    services.perform(task: tokenTask, session: session) { [weak self] result in
+                        switch result {
+                        case .success(let response):
+                            let token = Credential(
+                                type: CredentialType.token.rawValue,
+                                value: response.accessToken,
+                                expirationDate: Date().addingTimeInterval(TimeInterval(response.expiresIn))
+                            )
+                            var refresh: Credential?
+                            if let refreshToken = response.refreshToken {
+                                refresh = Credential(
+                                    type: CredentialType.refreshToken.rawValue,
+                                    value: refreshToken,
+                                    expirationDate: .distantFuture
+                                )
+                            }
+                            completion(.success([token, refresh].compactMap({ $0 })))
+                        case .failure(let error):
+                            completion(.failure(error))
+                        }
+                    }
+                }
+            )
+            
+            session.prefersEphemeralWebBrowserSession = true // TODO: should be specified in the app config
+            session.presentationContextProvider = strongSelf
+            
             return AppAuthFlowResumer(session: session)
         }
         let authenticationTask = AuthenticationTask(input: authProvider)
-        self.appAuthStep.services.perform(task: authenticationTask, session: self.appAuthStep.session) { [weak self] (response) in
+        self.appAuthStep.services.perform(task: authenticationTask, session: self.appAuthStep.session) { [weak self] response in
             DispatchQueue.main.async {
                 self?.hideLoading()
                 switch response {
@@ -79,28 +149,33 @@ extension MWAppAuthStepViewController {
     }
 }
 
-// MARK: - Authorization Controller Presentation
+extension MWAppAuthStepViewController: ASWebAuthenticationPresentationContextProviding {
+    
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        guard let window = self.view.window else { preconditionFailure() }
+        return window
+    }
+}
 
 extension MWAppAuthStepViewController: ASAuthorizationControllerPresentationContextProviding {
     
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        guard let windows = self.view.window else {
-            preconditionFailure()
-        }
-        
-        return windows
+        guard let window = self.view.window else { preconditionFailure() }
+        return window
     }
 }
 
 class AppAuthFlowResumer: AuthFlowResumer {
     
-    private var session: OIDExternalUserAgentSession?
+    private let session: ASWebAuthenticationSession
     
-    init(session: OIDExternalUserAgentSession) {
+    init(session: ASWebAuthenticationSession) {
         self.session = session
+        self.session.start()
     }
     
     func resumeAuth(with url: URL) -> Bool {
-        return self.session?.resumeExternalUserAgentFlow(with: url) ?? false
+        // ASWebAuthenticationSession doesn't need to be manually resumed
+        return true
     }
 }
