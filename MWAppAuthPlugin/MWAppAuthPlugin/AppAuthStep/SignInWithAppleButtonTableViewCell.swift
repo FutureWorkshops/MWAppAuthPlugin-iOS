4ca5d2f4864935de9521e7081927c8842a136ca9
Jonathan Flintham <jonathan@futureworkshops.com>
2022-06-22 17:05:16 +0100

    Removed hardcoded margins and corrected Theme

--- MWAppAuthPlugin/MWAppAuthPlugin/AppAuthStep/SignInWithAppleButtonTableViewCell.swift
+++ MWAppAuthPlugin/MWAppAuthPlugin/AppAuthStep/SignInWithAppleButtonTableViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AuthenticationServices
+import MobileWorkflowCore
 
 protocol SignInWithAppleButtonTableViewCellDelegate: AnyObject {
     func appleCell(_ cell: SignInWithAppleButtonTableViewCell, didTapButton button: UIButton)
@@ -17,6 +18,12 @@ final class SignInWithAppleButtonTableViewCell: UITableViewCell {
     private var loginButton: ASAuthorizationAppleIDButton?
     weak var delegate: SignInWithAppleButtonTableViewCellDelegate?
     
+    public var theme: Theme = .current {
+        didSet {
+            self.configureCell()
+        }
+    }
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.configureCell()
@@ -26,39 +33,35 @@ final class SignInWithAppleButtonTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configureCell() {
-        
-        self.backgroundColor = .secondarySystemBackground
-        self.contentView.backgroundColor = .secondarySystemBackground
-        
-        self.setupLoginButton()
-        self.setupConstraints()
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        self.configureCell()
     }
     
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        if previousTraitCollection?.userInterfaceStyle != self.traitCollection.userInterfaceStyle {
-            self.configureCell()
-        }
+    private func configureCell() {
+        
+        self.backgroundColor = self.theme.primaryBackgroundColor
+        self.contentView.backgroundColor = self.theme.primaryBackgroundColor
+        
+        self.setupLoginButton()
     }
     
     private func setupLoginButton() {
-        self.loginButton?.removeFromSuperview()
+        self.loginButton?.removeFromSuperview() // removes previous constraints
+        
         let loginButton = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: self.traitCollection.userInterfaceStyle == .dark ? .white : .black)
-        loginButton.cornerRadius = 14
+        loginButton.cornerRadius = self.theme.buttonCornerRadius
         loginButton.addTarget(self, action: #selector(self.didTapSignIn(_:)), for: .touchUpInside)
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         self.loginButton = loginButton
         self.contentView.addSubview(loginButton)
-    }
-    
-    private func setupConstraints() {
-        guard let loginButton = self.loginButton else { preconditionFailure() }
+        
         NSLayoutConstraint.activate([
-            loginButton.topAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.topAnchor, constant: 16),
-            loginButton.leftAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.leftAnchor, constant: 16),
-            loginButton.rightAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.rightAnchor, constant: -16),
-            loginButton.bottomAnchor.constraint(lessThanOrEqualTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            loginButton.heightAnchor.constraint(equalToConstant: 44)
+            loginButton.topAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.topAnchor, constant: 12),
+            loginButton.leftAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.leftAnchor, constant: 0),
+            loginButton.rightAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.rightAnchor, constant: 0),
+            loginButton.bottomAnchor.constraint(lessThanOrEqualTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: -12),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
